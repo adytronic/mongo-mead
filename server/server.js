16f511be941855c24f2d69cecb02a81bb4e2e54b
Adriana Jimenez <adytronic@gmail.com>
2017-03-20 22:58:35 -0700

    Add post /todos route to Postman check

--- server/server.js
+++ server/server.js
@@ -1,6 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todoModel');
 var {User} = require('./models/userModel');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var newTodo = new Todo({
+        text: req.body.text
+    });
+    newTodo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log('Listening on port ' + port);
+});
 
 // Todo new objects
 /*var newTodo = new models.Todo({
