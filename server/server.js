8430036fdf5d895ac9dacf862081b70ebf030113
Adriana Jimenez <adytronic@gmail.com>
2017-03-20 21:13:04 -0700

    Add user model in models folder

--- server/server.js
+++ server/server.js
@@ -1,23 +1,26 @@
 const mongoose = require('mongoose');
-
+const models = require('../models/models');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-var Todo = mongoose.model('Todo', {
-    text: {type: String},
-    completed: {type: Boolean},
-    completedAt: {type: Number}
-});
 
-var newTodo = new Todo({
-    text: 'Watch a baseball game',
-    completed: false,
-    completedAt: 100
+// Todo new objects
+/*var newTodo = new models.Todo({
+    text: 'Feed Sanson'
 });
-
 newTodo.save().then((doc) => {
     console.log(doc)
 }, (e) => {
-    console.log('Unable to save todo');
+    console.log(`Unable to save todo. ${e}`);
+});*/
+
+// User new objects
+var newUser = new models.User({
+    email: 'ajmangas@ajm.com'
+});
+newUser.save().then((doc) => {
+    console.log(doc)
+}, (e) => {
+    console.log(`Unable to save user. ${e}`);
 });
