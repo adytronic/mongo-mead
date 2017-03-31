2c95bc1caea054efc157b97a546d5bd7f78aadc5
Adriana Jimenez <adytronic@gmail.com>
2017-03-31 16:51:06 -0700

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 //const models = require('../models/models');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var uri = "mongodb://ajm_administrator:test@ds147510.mlab.com:47510/todo_app_mead";
+var url = uri || 'mongodb://localhost:27017/TodoApp';
+
+mongoose.connect(url);
 
 module.exports = {
     mongoose
